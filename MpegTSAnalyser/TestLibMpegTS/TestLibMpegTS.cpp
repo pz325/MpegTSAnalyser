2d34a295ab8efd4954c09bd004df68c6f74cb121
Ping Zou <sg71.cherub@gmail.com>
2015-08-06 20:14:52 +0100

    init the solution

---
+++ MpegTSAnalyser/TestLibMpegTS/TestLibMpegTS.cpp
@@ -0,0 +1,11 @@
+// TestLibMpegTS.cpp : Defines the entry point for the console application.
+//
+
+#include <gtest/gtest.h>
+
+int main(int argc, char* argv[])
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return 0;
+}
+
